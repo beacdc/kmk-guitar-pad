1d221abdb259dba9e3a2eb6e65c8cbc671699fb3
Beatriz Couto <beatrizcdc@gmail.com>
2024-02-20 18:58:09 -0300

    Changing mappings

--- code.py
+++ code.py
@@ -1,5 +1,3 @@
-print("Starting")
-
 import board
 
 from kmk.modules.layers import Layers
@@ -9,24 +7,21 @@ from kmk.scanners import DiodeOrientation
 from kmk.scanners.encoder import RotaryioEncoder
 from kmk.scanners.keypad import MatrixScanner
 from kmk.scanners.keypad import KeysScanner
-
-
-encoder_map = [KC.MW_UP, KC.MW_DOWN]
-matrix_map = [KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.ESPACE]
-single_key_map = [KC.B]
-
+from kmk.extensions.media_keys import MediaKeys
+from kmk.modules.mouse_keys import MouseKeys
 
 class MyKeyboard(KMKKeyboard):
-    def __init__(self):
+    def _init_(self):
 
         encoder = RotaryioEncoder(
             pin_a=board.GP14,
             pin_b=board.GP15,
+            divisor=2
         )
-        col_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5,)
+        col_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP7)
         row_pins = (board.GP29,)
         diode_orientation = DiodeOrientation.COL2ROW
-        key_config = [board.GP26]
+        key_config = [board.GP26, board.GP8]
         key_scanner = KeysScanner(
             # require argument:
             pins=key_config,
@@ -45,10 +40,22 @@ class MyKeyboard(KMKKeyboard):
 
 
 keyboard = MyKeyboard()
+keyboard.extensions.append(MediaKeys())
+keyboard.modules.append(Layers())
+keyboard.modules.append(MouseKeys())
 
-# keyboard.modules.append(Layers())
-keyboard.keymap = [matrix_map + single_key_map + encoder_map]
+encoder_map_l0 = [KC.MW_UP, KC.MW_DOWN]
+matrix_map_l0 = [KC.SPACE, KC.N5, KC.N4, KC.N3, KC.N2, KC.N1]
+single_key_map_l0 = [KC.TO(1), KC.B]
 
 
-if __name__ == '__main__':
+encoder_map_l1 = [KC.VOLU, KC.VOLD]
+matrix_map_l1 = [KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS, KC.TRNS]
+single_key_map_l1 = [KC.TO(0), KC.MSTP]
+
+keyboard.keymap = [(matrix_map_l0 + single_key_map_l0 + encoder_map_l0),
+                   (matrix_map_l1 + single_key_map_l1 + encoder_map_l1)]
+
+
+if __name__ == '_main_':
     keyboard.go()
